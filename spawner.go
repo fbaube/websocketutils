e37522e97360d2a204cd31cfaf9537e4a5e46ac0
Fred Baube <fbaube@iki.fi>
2023-11-19 17:11:27 +0200

    Add cool utility func SpawnUpgradedWSs

---
+++ spawner.go
@@ -0,0 +1,42 @@
+package websocketutils
+
+import (
+	"net/http"
+	"fmt"
+	WS "nhooyr.io/websocket"
+	// "nhooyr.io/websocket/wsjson"
+	// "crypto/tls"
+)
+
+type UpgradedWebsocketHandler func(*WSUConn) 
+// func(p *WS.Conn)(*WSU.WSUConn, error)
+
+var GlobalUpgradedWebsocketHandler UpgradedWebsocketHandler
+
+func SpawnUpgradedWSs(w http.ResponseWriter, req *http.Request) { 
+	// Accept(w, req, opts *AcceptOptions) (*Conn, error)
+	// Accept accepts a WebSocket handshake from a client
+	//    and upgrades the the connection to a WebSocket.
+	// Accept will not allow cross origin requests by default.
+	// Accept will write a response to w on all errors.
+	var wsCn *WS.Conn
+	var err error 
+	wsCn, err = WS.Accept(w, req, nil) // nil is AcceptOptions 
+	if err != nil {
+		fmt.Printf("Spawner: Accept failed: %s \n", err.Error())
+		return 
+	}
+	
+	// "Upgrade"
+	wsuCn := new(WSUConn)
+	wsuCn.Conn = wsCn
+	// Make a net.Conn and get info from it
+	nc := WS.NetConn(DefaultCtx, wsCn, WS.MessageText)
+	la := nc.LocalAddr()
+	ra := nc.RemoteAddr()
+	fmt.Printf("Spawned: local<%s:%s> remote<%s:%s> \n",
+		la.Network(), la.String(), ra.Network(), ra.String())
+
+	go GlobalUpgradedWebsocketHandler(wsuCn) 
+}
+
